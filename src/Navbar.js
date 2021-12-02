3bd6433e21e39d969cb1f6df0beb3022da565fb4
Fatma-MahmoudAli <91899509+Fatma-MahmoudAli@users.noreply.github.com>
2021-12-02 21:01:18 +0200

    Add files via upload

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,13 @@
 
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 const Nav = () => {
     return ( 
         <header  >
         
         <div class="menutoggle" onclick="togglemenu()"></div>
         <ul class="navigation">
-            <li><a href="home.html" onclick="togglemenu()">Home</a></li>
+        <li><a href="home.html" onclick="togglemenu()">Home</a></li>
             <li><a href="#about" onclick="togglemenu()">About</a></li>
             {/* <Link to="/AllMenu">Mnue</Link> */}
             <li><a href="#Sport" onclick="togglemenu()">Sport</a></li>
@@ -21,3 +21,27 @@ const Nav = () => {
 }
  
 export default Nav;
+
+
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// const Nav = () => {
+//     return ( 
+//         <nav className="navbar">
+//       <h1>The Dojo Blog</h1>
+//       <div className="links">
+//         <Link to="/">Home</Link>
+//         <Link to="/create">Create</Link>
+//         <Link to="/menu" style={{ 
+//           color: 'white', 
+//           backgroundColor: '#f1356d',
+//           borderRadius: '8px' 
+//         }}>New Blog</Link>
+//       </div>
+//     </nav>
+
+//      );
+// }
+ 
+// export default Nav;
